c3d624fbd2952ff2e22b397b5540fba9ee2ff5c2
Brian Brietzke <bbrietzke+github@gmail.com>
2023-08-24 05:08:35 -0500

    Initial Import of Spring starter.

---
+++ src/main/java/io/faultycloud/apps/LingeringMoon/LingeringMoonApplication.java
@@ -0,0 +1,13 @@
+package io.faultycloud.apps.LingeringMoon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LingeringMoonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LingeringMoonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/faultycloud/apps/LingeringMoon/LingeringMoonApplicationTests.java
@@ -0,0 +1,13 @@
+package io.faultycloud.apps.LingeringMoon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LingeringMoonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
